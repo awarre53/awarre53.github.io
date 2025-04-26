fb62131b4a228c7edcb3fe3fc94bfd1d6a2e4768
awarre53 <austinw7@icloud.com>
2025-04-26 16:44:17 -0400

    Drawing (p5)

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(710, 400);
+    background(1000);
+    strokeWeight(8);
+    colorMode(HSB);
+    describe('A blank canvas where the user draws by dragging the mouse');
+}
+  
+function mouseDragged() {
+    let lineHue = mouseX - mouseY;
+    stroke(lineHue, 90, 90);
+    line(pmouseX, pmouseY, mouseX, mouseY);
+}
+
+const resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", () => {
+    setup();
+});
